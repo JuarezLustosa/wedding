1a6b4369fab0f192d87f5e6b5e090d6807e9a83e
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-04-12 12:19:18 -0300

    Adicionando os assests necessario para o production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,9 @@ Wedding::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  
+  config.assets.precompile += %w(countdown_dashboard.js home.js)
+  
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
