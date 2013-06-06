60c9266b2de175314af2d697e1d8a0f2725fb1d8
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-06-06 10:16:05 -0300

    More images

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,17 @@
-!function ($) {
-  $(function(){
-    $('#myCarousel').carousel()
-  })
-}(window.jQuery)
+$('#myCarousel').carousel()
+
+var photo = function() {
+  var paths = ['assets/cris.jpg', 
+               'assets/fernando.jpg', 
+               'assets/tahisa.jpg', 
+               'assets/maira.jpg',
+               'assets/consuelo.jpg']
+               
+  var randon_path = paths[Math.floor(Math.random()*paths.length)]
+  
+  $('.image').children().attr('src', randon_path)
+
+  setTimeout(photo, 5000);
+}
+
+photo();
